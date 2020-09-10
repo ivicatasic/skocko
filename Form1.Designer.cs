8d84d623be78de35f27506c28295ca2f02a4281f
ivicatasic <ivica.tasic@pmf.edu.rs>
2020-09-10 21:17:49 +0200

    Add project

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace skocko
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "8";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.60137F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.05695F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 8;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.46753F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.46753F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(205, 340);
+            this.tableLayoutPanel2.TabIndex = 2;
+            
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 4;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(433, 0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 8;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(193, 340);
+            this.tableLayoutPanel3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(281, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(225, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Broj pokusaja: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(325, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 340);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace skocko
+{
+    public partial class Form1 : Form
+    {
+
+        public Button[,] polja = new Button[8, 4];
+        public static int jj;
+        public Button[,] krugovi = new Button[8, 4];
+        public int brojac;
+        Color[] boje = new Color[6];
+        Random r = new Random();
+        public int brPoteza;
+        Color[] kombinacija = new Color[4];
+        public bool pogodjenPrvi;
+        public bool pogodjenDrugi;
+        public bool pogodjenTreci;
+        public bool pogodjenCetvrti;
+
+        public Form1()
+        {
+            InitializeComponent();
+            brPoteza = 8;
+            brojac = 0;
+            jj = 0;
+            pogodjenPrvi = false;
+            pogodjenDrugi = false;
+            pogodjenTreci = false;
+            pogodjenCetvrti = false;
+
+            boje[0] = Color.Black;
+            boje[1] = Color.Blue;
+            boje[2] = Color.Yellow;
+            boje[3] = Color.Green;
+            boje[4] = Color.Red;
+            boje[5] = Color.LightGray;
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    polja[i, j] = new Button();
+                    polja[i, j].Dock = System.Windows.Forms.DockStyle.Fill;
+                    polja[i, j].Location = new System.Drawing.Point(3, 3);
+                    polja[i, j].Size = new System.Drawing.Size(26, 42);
+                    polja[i, j].Name = i.ToString() + " " + j.ToString();
+                    polja[i, j].TabIndex = 0;
+                    
+                    polja[i, j].UseVisualStyleBackColor = true;
+                    this.tableLayoutPanel2.Controls.Add(polja[i, j], j, i);
+                    polja[i, j].Click += new System.EventHandler(this.dugme_Click);
+                }
+            }
+
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    krugovi[i, j] = new Button();
+                    krugovi[i, j].Dock = System.Windows.Forms.DockStyle.Fill;
+                    krugovi[i, j].Location = new System.Drawing.Point(3, 3);
+                    krugovi[i, j].Size = new System.Drawing.Size(26, 42);
+                    krugovi[i, j].Name = i.ToString() + " " + j.ToString();
+                    krugovi[i, j].TabIndex = 0;
+
+                    krugovi[i, j].UseVisualStyleBackColor = true;
+                    this.tableLayoutPanel3.Controls.Add(krugovi[i, j], j, i);
+                    //polja[i, j].Click += new System.EventHandler(this.dugme_Click);
+                }
+            }
+
+        }
+
+        private void dugme_Click(object sender, EventArgs e)
+        {
+            
+
+            Button dugme = sender as Button;
+            string s = dugme.Name;
+            string[] sar = s.Split(' ');
+            int i = Convert.ToInt32(sar[0]);
+            int j = Convert.ToInt32(sar[1]);
+            polja[i, j].BackColor = boje[brojac];
+            if (brojac == 5)
+            {
+                brojac = 0;
+            }
+            else
+            {
+                brojac++;
+            }
+
+        }
+
+        public Color [] generisiKombinaciju()
+        {
+            Color[] kominacija = new Color[4];
+            int p1 = r.Next(0, 6);
+            kominacija[0] = boje[p1];
+            int p2 = r.Next(0, 6);
+            kominacija[1] = boje[p2];
+            int p3 = r.Next(0, 6);
+            kominacija[2] = boje[p3];
+            int p4 = r.Next(0, 6);
+            kominacija[3] = boje[p4];
+
+            return kominacija;
+        }
+         
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label3.Text = brPoteza.ToString();
+            kombinacija = generisiKombinaciju();
+        }
+
+        public void updateLabelu()
+        {
+            label3.Text = brPoteza.ToString();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (brPoteza > 0 && jj<=7)
+            {
+            for(int ii = 0; ii < 4; ii++)
+                {
+                    if (ii == 0)
+                    {
+                        if (kombinacija[ii] == polja[jj, ii].BackColor)
+                        {
+                            pogodjenPrvi = true;
+                        }
+                    }
+                    else if (ii == 1)
+                    {
+                        if (kombinacija[ii] == polja[jj, ii].BackColor)
+                        {
+                            pogodjenDrugi = true;
+                        }
+                    }
+                    else if (ii == 2)
+                    {
+                        if (kombinacija[ii] == polja[jj, ii].BackColor)
+                        {
+                            pogodjenTreci = true;
+                        }
+                    }
+                    else if (ii == 3)
+                    {
+                        if (kombinacija[ii] == polja[jj, ii].BackColor)
+                        {
+                            pogodjenCetvrti = true;
+                        }
+                    }
+                }
+                
+
+            }
+            else
+            {
+                MessageBox.Show("Nemate vise pokusaja!");
+                Close();
+            }
+
+            for(int p = 0; p < 4; p++)
+            {
+                if (p == 0)
+                {
+                    if (pogodjenPrvi)
+                    {
+                        krugovi[jj, p].BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        krugovi[jj, p].BackColor = Color.Gray;
+                    }
+                }
+                else if (p== 1)
+                {
+                    if (pogodjenDrugi)
+                    {
+                        krugovi[jj, p].BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        krugovi[jj, p].BackColor = Color.Gray;
+                    }
+                }
+                else if (p == 2)
+                {
+                    if (pogodjenTreci)
+                    {
+                        krugovi[jj, p].BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        krugovi[jj, p].BackColor = Color.Gray;
+                    }
+                }
+                else if (p == 3)
+                {
+                    if (pogodjenCetvrti)
+                    {
+                        krugovi[jj, p].BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        krugovi[jj, p].BackColor = Color.Gray;
+                    }
+                }
+            }
+
+            if(pogodjenPrvi && pogodjenDrugi && pogodjenTreci && pogodjenCetvrti)
+            {
+                MessageBox.Show("Uspesno ste pogodili kombinaciju!");
+            }
+
+            brPoteza--;
+            updateLabelu();
+
+            pogodjenPrvi = false;
+            pogodjenDrugi = false;
+            pogodjenTreci = false;
+            pogodjenCetvrti = false;
+            jj = jj + 1;
+        }
+    }
+}
